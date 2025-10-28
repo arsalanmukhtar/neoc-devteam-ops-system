ffecdc2e7ff78afec7134fd76181465964eab81d
arsalanmukhtar <arsahlahn3@gmail.com>
2025-10-28 15:53:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-center p-4">
+      Hello React + Vite + Tailwind!
+    </div>
+  );
+}
+export default App;
